6c09261cb87cb4d1171d1aa992cc777c4f4ebf2a
Urur49 <ururKM493@bk.ru>
2020-01-15 08:23:43 +0100

    My WTapp,third commit

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -4,10 +4,11 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q5i*1y15y#u#s@4+zv-p7p=9@zgp2di0+2jbn9=-10=^7q2nh#'
+#SECRET_KEY = 'q5i*1y15y#u#s@4+zv-p7p=9@zgp2di0+2jbn9=-10=^7q2nh#'
+SECRET_KEY = '5d8c7c90fd156917c2370984c89ba78594dcf340'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
